eca5b9422d4476bf729006ab8c08498d7b292fdf
amalub911 <151335054+amalub911@users.noreply.github.com>
2026-03-21 19:15:00 +0530

    hi

--- Scripts/mechanic_ui.gd
+++ Scripts/mechanic_ui.gd
@@ -1,9 +1,13 @@
 extends Control
 
+# --- VARIABLES ---
 @onready var health_label = $HealthLabel
+@onready var health_bar = $HUD/ShipHealthBar
 
 # The NetworkManager will knock on this door, and this script will answer!
 func update_health_ui(new_health: int):
+	
+	# 1. Update the Text Label
 	if health_label != null:
 		health_label.text = "Ship Health: " + str(new_health) + "%"
 		
@@ -11,3 +15,7 @@ func update_health_ui(new_health: int):
 			health_label.add_theme_color_override("font_color", Color.RED)
 		else:
 			health_label.add_theme_color_override("font_color", Color.WHITE)
+			
+	# 2. Update the Visual Progress Bar
+	if health_bar != null:
+		health_bar.value = new_health
